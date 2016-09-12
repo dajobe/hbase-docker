dc98072d1788ea672ff723abb5f76f5a39cebda4
Dave Beckett <dave@dajobe.org>
2016-09-12 08:39:14 -0700

    apache dist url; local to me at least

--- config-hbase.sh
+++ config-hbase.sh
@@ -2,10 +2,13 @@
 
 # . /build/config.sh
 
+HBASE_DIST="http://apache.cs.utah.edu/hbase"
+#HBASE_DIST="http://archive.apache.org/dist/hbase"
+
 # Prevent initramfs updates from trying to run grub and lilo.
 export INITRD=no
 export DEBIAN_FRONTEND=noninteractive
-export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
 
 minimal_apt_get_args='-y --no-install-recommends'
 
