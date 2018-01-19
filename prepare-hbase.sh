2b0ce69ad1bbb3ec6d9b00da29bf8df30cb92923
mbarison <eltorpe@hotmail.com>
2018-01-19 10:08:31 -0500

    * Changed version 1.2.6 * Use dispatcher to get closest mirror site * Base on openjdk:8-jre-alpine image * GPG download authentication

--- prepare-hbase.sh
+++ prepare-hbase.sh
@@ -1,11 +1,29 @@
-#!/bin/sh -x
+#!/bin/sh -xe
 
 . /build/config-hbase.sh
 
-apt-get update -y
+apk update
+apk add bash $HBASE_BUILD_PACKAGES
 
-apt-get install $minimal_apt_get_args $HBASE_BUILD_PACKAGES
-
+mkdir -p /opt
 cd /opt
 
-curl -SL $HBASE_DIST/$HBASE_VERSION/hbase-$HBASE_VERSION-bin.tar.gz | tar -x -z && mv hbase-${HBASE_VERSION} hbase
+if [ -n $USE_DISPATCHER ]
+then
+  HBASE_DIST=$(get_closest_site)
+fi
+
+# download package
+curl -SLO $HBASE_DIST/$HBASE_VERSION/hbase-$HBASE_VERSION-bin.tar.gz 
+# download PGP keys
+curl -SLO $HBASE_ORIG_DIST/KEYS 
+# download checksum
+curl -SL $HBASE_ORIG_DIST/$HBASE_VERSION/hbase-$HBASE_VERSION-bin.tar.gz.asc -o download.asc
+
+# validate the version
+gpg --import KEYS
+gpg --verify download.asc hbase-$HBASE_VERSION-bin.tar.gz
+
+# finally unpack and cleanup
+tar xzf hbase-$HBASE_VERSION-bin.tar.gz && mv hbase-${HBASE_VERSION} hbase
+rm -f hbase-$HBASE_VERSION-bin.tar.gz KEYS download.asc
